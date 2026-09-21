2f6f7f518797db1c44963e1fcb80af8b3e267a86
MergerCo <info.mergerconsulting@gmail.com>
2026-09-21 02:36:44 -0400

    Update artwork titles and descriptions

--- artworks.js
+++ artworks.js
@@ -1,14 +1,22 @@
 // artworks.js
 const artworks = [
   {
-    titulo: "Ondas",
-    archivo: "waves.html",
-    descripcion: "Exploración de ruido Perlin y ondas sinusoidales."
+    titulo: "Noise Currents",
+    archivo: "1.html",
+    descripcion: "1,000 particles move along an organic noise field."
   },
+
   {
-    titulo: "Partículas",
-    archivo: "particles.html",
-    descripcion: "Sistema de partículas con atracción gravitatoria."
+    titulo: "Free Willy",
+    archivo: "2.html",
+    descripcion: "A generative orca built from thousands of 3D points swims through a dark ocean."
   },
+
+  {
+    titulo: "Terra Flow",
+    archivo: "3.html",
+    descripcion: "Thousands of particles drift along animated 3D noise currents, picking up colors from an imaginary planet's biomes."
+  },
+  
   // Añade más obras aquí...
 ];
